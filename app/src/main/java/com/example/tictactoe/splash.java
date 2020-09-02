3f0768e1705c3cb3d05ce994d12e3c088c45381f
altruisticdream <ashutoshgautam9628@outlook.com>
2020-09-02 16:12:05 +0530

    first game

---
+++ app/src/main/java/com/example/tictactoe/splash.java
@@ -0,0 +1,24 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent homeIntent= new Intent(splash.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
